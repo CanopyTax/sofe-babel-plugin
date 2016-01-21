3d54b4272f618e00643ea60822631095555404dd
Joel Denning <joeldenning@gmail.com>
2016-01-20 23:41:02 -0700

    Adding runtime.js

---
+++ src/runtime.js
@@ -0,0 +1,19 @@
+export function loadServices(services) {
+	if (!Array.isArray(services))
+		throw new Error(`services must be an array of strings`);
+	if (!window.System)
+		throw new Error(`SystemJS and sofe must be loaded`);
+	window.__synchronousSofe__ = {};
+	return Promise.all(services.map((serviceName) => {
+		return new Promise((resolve, reject) => {
+			window.System.import(`${serviceName}!sofe`)
+			.then((service) => {
+				window.__synchronousSofe__[serviceName] = service;
+				resolve();
+			})
+			.catch((ex) => {
+				reject(ex);
+			});
+		})
+	}));
+}
